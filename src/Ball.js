deb71bc5f9d18434c5797ac21bc31fe0b7e8b784
ywCN <wangyouhao90@gmail.com>
2018-01-10 18:46:25 -0500

    comment

--- src/Ball.js
+++ src/Ball.js
@@ -3,11 +3,14 @@ import { View, Animated } from 'react-native';
 
 class Ball extends Component {
     componentWillMount() {
-        this.position = new Animated.ValueXY(0, 0);
+        // What is the current position of the element being animated?
+        this.position = new Animated.ValueXY(0, 0); // start: 0, 0
+        // spring function changes the value of position(x,y) over some time
         Animated.spring(this.position, {
-            toValue: { x: 200, y: 500 }
-        }).start();
+            toValue: { x: 200, y: 500 } // move to: 200, 500
+        }).start(); // start this operation now
     }
+
     render() {
         return (
             <Animated.View style={this.position.getLayout()}>
