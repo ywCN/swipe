d322226ded256838b8db2b79d1923899b6937b50
ywCN <wangyouhao90@gmail.com>
2018-01-10 21:41:33 -0500

    import and use Deck

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Ball from './src/Ball';
+import Deck from './src/Deck';
 
 export default class App extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Ball />
+                <Deck />
             </View>
         );
     }
