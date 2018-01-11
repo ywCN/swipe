3259be90c2d2fb16f4ecdfecc39b3c2968f955ac
ywCN <wangyouhao90@gmail.com>
2018-01-10 19:07:11 -0500

    flow of rendering

--- src/Ball.js
+++ src/Ball.js
@@ -1,11 +1,17 @@
+// Flow of rendering
+// 1. Ball and Animated.View rendered
+// 2. Animated.View inspects its props, finds animated value
+// 3. ValueXY starts changing
+// 4. Animated.View sees updated value from ValueXY
+// 5. Animated.View updates its styling
 import React, { Component } from 'react';
 import { View, Animated } from 'react-native';
 
 class Ball extends Component {
     componentWillMount() {
-        // What is the current position of the element being animated?
+        // 1. What is the current position of the element being animated?
         this.position = new Animated.ValueXY(0, 0); // start: 0, 0
-        // How is the animation changing?
+        // 2. How is the animation changing?
         // spring function changes the value of position(x,y) over some time
         Animated.spring(this.position, {
             toValue: { x: 200, y: 500 } // move to: 200, 500
@@ -14,7 +20,7 @@ class Ball extends Component {
 
     render() {
         return (
-            // Apply the animation's current position to an actual component.
+            // 3. Apply the animation's current position to an actual component.
             // Animated.View applies position to Ball.
             <Animated.View style={this.position.getLayout()}>
                 <View style={styles.ball} />
